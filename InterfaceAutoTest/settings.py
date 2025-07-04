81bfde8d5e6f5d12c93fbfc98aef634bb7f244cb
Chenhan <han.chen035969@baozun.com>
2025-07-04 17:44:34 +0800

    修改redis_host

--- InterfaceAutoTest/settings.py
+++ InterfaceAutoTest/settings.py
@@ -129,4 +129,4 @@ STATICFILES_DIRS = [
     os.path.join(BASE_DIR, "static"),
 ]
 redis_port = 6379
-redis_host = '127.0.0.1'
+redis_host = 'redis'
--- interfacetestplatform/tests.py
+++ interfacetestplatform/tests.py
@@ -1,16 +1,19 @@
-import requests
+import multiprocessing
 
-# 简化的GraphQL请求
-payload = {"operationName":"registerAndBindGql","variables":{"input":{"iv":"M1BYSjBWS05sT3lQZlczeg==","encryptedData":"BH8bNmcc3kdBfjp0jUrYo5qUu3e4BTqx66sK2MNVNQPR0owu7CCQYuJBSQsjo+SCiMwwOoofsRNbwoKhNy/4ALV2wogpyQREAYJtuhnQKGd+pyRmt9XXDLb8oGq1oZvnSIlVgoevNZJxhZhC3b7BEKRwuZXS0+gPy45oA73LUZdWgcA7Ywu5DOew5nWWd+pw8lgRON5F0ywbA8OVc6163A==","source":"13","openId":"716fca94a19ed1bf8bb301cbcbec6abf6b19d566c6aeea53eb1431cee983b131","crmUtm":{"utmCampaign":"","utmContent":"","utmMedium":"","utmSource":"","utmTerm":""}}},"extensions":{"persistedQuery":{"version":1,"sha256Hash":"90dcf5203b772bfdab7f02b5a97eb16579ddcaf95648e385f78f732e06c7bbe5"}}}
+a = 1
 
-response = requests.post(
-    url="https://rmp.staging.lululemon.cn/api/graphql/registerAndBindGql",
-    json=payload,
-    headers={
-        "content-type": "application/json",
-        "unex-user-token": "eyJhbGciOiJIUzI1NiJ9.eyJhY2NvdW50RXh0UHJvcHMiOm51bGwsImFjY291bnRObyI6IkdVRVNULWx1bHVsZW1vbi0xOTM3Nzg3ODkwMTY3ODE3IiwiYWNjb3VudFJlYWxtQ29kZSI6bnVsbCwiY2hhbm5lbENvZGUiOm51bGwsImVtYWlsIjpudWxsLCJleHQiOm51bGwsIm1vYmlsZSI6bnVsbCwibW9kZSI6bnVsbCwibW9kZVZhbHVlIjpudWxsLCJzYWFzVGVuYW50Q29kZSI6bnVsbCwic291cmNlIjpudWxsLCJzdGF0dXMiOm51bGwsInN0b3JlQ29kZSI6bnVsbCwidG9rZW4iOm51bGwsImZpcnN0TG9naW4iOm51bGwsImxhc3RMb2dpblRpbWUiOm51bGwsIm9wZW5JZCI6bnVsbCwidW5pb25JZCI6bnVsbCwidmlzaXRvciI6dHJ1ZX0.O-LFby7DniUuG0k6IKTFeUcv1kQWB3sO-feti8xfUsY"
-    }
-)
+def demo1():
+    global a
+    a += 1
 
-print(response.status_code)
-print(response.json())
+def demo2():
+    print(a)
+
+def main():
+    t1 = multiprocessing.Process(target=demo1)
+    t2 = multiprocessing.Process(target=demo2)
+    t1.start()
+    t2.start()
+
+if __name__ == '__main__':
+    main()
